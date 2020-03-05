f57e0eee4dccc898bc9634a6645f45f2177f7e15
shinghal.swati <shinghal.swati@gmail.com>
2020-03-05 11:19:28 +0530

    adding basic rest services in python

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,51 @@
-from flask import Flask
+# Using flask to make an api 
+# import necessary libraries and functions 
+from flask import Flask, jsonify, request
 
+# creating a Flask app 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
 
+# on the terminal type: curl http://127.0.0.1:5000/
+# returns hello world when we use GET. 
+# returns the data that we send when we use POST. 
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if (request.method == 'GET'):
+        data = "hello world"
+        return jsonify({'data': data})
 
+    # A simple function to calculate the square of a number
+# the number to be squared is sent in the URL when we use GET 
+# on the terminal type: curl http://127.0.0.1:5000 / home / 10
+# this returns 100 (square of 10)
+@app.route('/home/<int:num>', methods=['GET'])
+def disp(num):
+    return jsonify({'data': num ** 2})
+
+@app.route('/siemens', methods=['GET'])
+def getsiemensdata():
+    return jsonify({'new': 5,
+                    'technology': 10,
+                    'marketTrend': 15})
+
+@app.route('/abb', methods=['GET'])
+def getabbdata():
+    return jsonify({'new': 5,
+                    'technology': 10,
+                    'marketTrend': 15})
+@app.route('/schinder', methods=['GET'])
+def getschinderdata():
+    return jsonify({'new': 5,
+                    'technology': 10,
+                    'marketTrend': 15})
+
+@app.route('/rockwell', methods=['GET'])
+def getrockwelldata():
+    return jsonify({'new': 5,
+                    'technology': 10,
+                    'marketTrend': 15})
+
+# driver function
 if __name__ == '__main__':
-    app.run(port=9999)
+    app.run(debug=True)
